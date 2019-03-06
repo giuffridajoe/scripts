376ee5f5a18b2988258806159e5e0df723be130b
Joseph Giuffrida Jr <giuffridajoe@gmail.com>
2019-03-05 20:55:19 -0500

    Update sox-script.sh
    updated with dummy file and directory paths

--- sox-script.sh
+++ sox-script.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 #sox first.mp3 second.mp3 third.mp3 result.mp3
 while IFS="," read column1 column2 column3; do
-sox $column1 $column2 /opt/rai/remote/voice/English/joe/$column3;
-done < joe/sox-wavs.txt
+sox $column1 $column2 /path to file/$column3;
+done < /directory to save new file/sox-wavs.txt
