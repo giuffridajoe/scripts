38fa92cc5ba9d60791af47db127b40151de3deda
Joe Giuffrida <giuffridajoe@gmail.com>
2019-04-18 21:28:40 -0400

    breaks up a big file into smaller ones

--- split-file.sh
+++ split-file.sh
@@ -2,8 +2,8 @@
 
 #split into smaller files containing this many lines per file and name each file
 #note: each file will be called filenameaa, filenameab etc.
-split -l 15000 $1 [insert filename for split files]
+split -l 15000 $1 test-test 
 #iterate over each new file and prepend this text to the beginning
-for file in [files to iterate over]*; do echo '[add text here]' | cat - "$file" > temp && mv temp "$file"; done
+for file in test*; do echo 'add text here' | cat - "$file" > temp && mv temp "$file"; done
 #iterate over each new file and append a file extension
-for file in [files to iterate over]*; do mv -- "$file" "$file.txt"; done
+for file in test*; do mv -- "$file" "$file.txt"; done
