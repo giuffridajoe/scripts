58c4137d296ef970498fbf495f750b10f03ee116
Joe Giuffrida <giuffridajoe@gmail.com>
2019-04-18 20:32:05 -0400

    command line parameter for file to split

--- split-file.sh
+++ split-file.sh
@@ -2,8 +2,8 @@
 
 #split into smaller files containing this many lines per file and name each file
 #note: each file will be called filenameaa, filenameab etc.
-split -l 15000 [insert filename here] [insert filename for split files]
-#iterate over each file and prepend this text to the beginning
+split -l 15000 $1 [insert filename for split files]
+#iterate over each new file and prepend this text to the beginning
 for file in [files to iterate over]*; do echo '[add text here]' | cat - "$file" > temp && mv temp "$file"; done
-#iterate over each file and append a file extension
+#iterate over each new file and append a file extension
 for file in [files to iterate over]*; do mv -- "$file" "$file.txt"; done
